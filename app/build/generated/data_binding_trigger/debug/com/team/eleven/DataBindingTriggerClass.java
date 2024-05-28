75f50c4b5160feb559da656fc2d543cb326c5171
Aman Kapoor <kapooraman32@gmail.com>
2024-05-29 03:10:00 +0530

    Initial commit

---
+++ app/build/generated/data_binding_trigger/debug/com/team/eleven/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.team.eleven;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ app/src/main/java/com/team/eleven/AppConfig.java
@@ -0,0 +1,6 @@
+package com.team.eleven;
+
+public class AppConfig {
+    public static String ApiUrl = "YOUR_API_URL";
+    public static String ApiKey = "YOUR_API_KEY";
+}
